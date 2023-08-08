a396dcb4e8f0c00ee3a10b7ac71199011fe873ab
seejnn <seejinkim@gmail.com>
2023-08-08 22:47:46 +0900

    home layout

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,20 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
 
 function App() {
 
 
   return (
     <>
-      <h1 className="text-3xl font-bold text-blue-700">테스트</h1>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>    
+      
+
     </>
   )
 }
---
+++ react/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link, Outlet } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <div className="sticky top-0 z-50">
+      <nav className="sticky top-0 w-full bg-white/70 backdrop-blur-sm ring-1 ring-slate-100 shadow-sm py-4 px-6 justify-center items-center  ">
+        <ul className="flex gap-4 justify-center text-slate-400 transition-all">
+          <li>
+            <Link to='/' className="hover:text-slate-800  transition-all p-3">홈</Link>
+          </li>
+          <li>
+            <Link to='#' className="hover:text-slate-800  transition-all p-3">행사 정보</Link>
+          </li>
+          <li>
+            <Link to='#' className="hover:text-slate-800  transition-all p-3">마이 페이지</Link>
+          </li>
+        </ul>
+        <Outlet />
+      </nav>
+    </div>
+  )
+}
