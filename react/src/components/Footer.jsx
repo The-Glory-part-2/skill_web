4bb9578ba8de8395aa8cd304c3b6c3b7eadb186c
seejnn <seejinkim@gmail.com>
2023-08-09 23:55:07 +0900

    add login (merge)

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="w-full h-[10vh] bg-gray-900 text-white flex text-xl font-bold text-center mt-[100px]">
+    <div className="w-full h-[10vh] bg-gray-900 text-white flex text-xl font-bold text-center">
       <h2>로고</h2>
     </div>
   )
