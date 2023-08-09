317c07aee8b5bae029ecf78bea153873676ddf21
seejnn <seejinkim@gmail.com>
2023-08-09 19:52:52 +0900

    add mypage

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,24 +1,28 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Board from './pages/Board'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Board from "./pages/Board";
+import Mypage from "./pages/Mypage";
+import Footer from "./components/Footer";
+
+
+
 
 
 function App() {
-
-
   return (
     <>
       <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='board' element={<Board />} />
-        </Routes>    
-      
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="board" element={<Board />} />
+        <Route path="board/:id" element={<Board />} />
+        <Route path="mypage" element={<Mypage />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
 
 export default function Navbar() {
   return (
     <div className="sticky top-0 z-50">
-      <nav className="sticky top-0 w-full bg-white/70 backdrop-blur-sm ring-1 ring-slate-100 shadow-sm py-4 px-6 justify-center items-center  ">
-        <ul className="flex gap-4 justify-center text-slate-400 transition-all">
-          <li>
+      <nav className="sticky flex flex-row top-0 w-full bg-white/70 backdrop-blur-sm ring-1 ring-slate-100 shadow-sm py-4 px-6 justify-center items-center">
+        <Link to='/' className="basis-1/4">
+          <h1 className="text-lg font-bold">로고</h1>
+        </Link>
+        <ul className="flex flex-row gap-4 basis-2/4 justify-center text-slate-400 transition-all">
+          {/* <li>
             <Link to="/" className="hover:text-slate-800  transition-all p-3">
               홈
             </Link>
-          </li>
+          </li> */}
           <li>
             <Link to="#" className="hover:text-slate-800  transition-all p-3">
               행사 정보
@@ -20,8 +24,11 @@ export default function Navbar() {
               게시판
             </Link>
           </li>
-        </ul>
-        <Outlet />
+        </ul> 
+        <Link to='mypage' className="text-slate-500 justify-end flex basis-1/4 items-center gap-2">
+          <UserCircleIcon className="w-5 h-5"/>
+          <span>홍길동님</span>
+        </Link>
       </nav>
     </div>
   );
