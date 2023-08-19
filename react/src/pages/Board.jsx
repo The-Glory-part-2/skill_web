738bf96e6af14a341ab93a8356346895ec7dd626
seejnn <seejinkim@gmail.com>
2023-08-19 20:01:23 +0900

    test

--- react/src/pages/Board.jsx
+++ react/src/pages/Board.jsx
@@ -3,7 +3,7 @@ const posts = [
   { id: 2, title: "두 번째 게시글", content: "두 번째 게시글 내용입니다." },
   { id: 3, title: "세 번째 게시글", content: "세 번째 게시글 내용입니다." }
 ];
-
+// dsdfsdf
 export default function Board() {
   return (
     <div className="container mx-auto max-w-4xl mt-12 h-[1000px]">
